aa6feeb9b31b7e8e7cf0eabbbb1268da8ae94450
Haruka Asakura <haruka.asakura@esm.co.jp>
2019-03-14 12:51:09 +0900

    feat: idobot-gopher added to sample

---
+++ sample/idobot-gopher/go.mod
@@ -0,0 +1,9 @@
+module github.com/mk2/idobot-gopher
+
+require (
+	github.com/mk2/idobot v0.0.0-20190310063854-ef7852a2264e
+	github.com/r3labs/sse v0.0.0-20181217150409-243b7807c4c4 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20190310014029-b774fd8d5c0f // indirect
+	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
+)
---
+++ sample/idobot-gopher/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/mk2/idobot"
+)
+
+const (
+	idobataURL    = "https://idobata.io"
+	userAgent     = "gopher-bot"
+	playGroundURL = "https://play.golang.org"
+)
+
+// FormatResp Playgroundからのレスポンス
+type FormatResp struct {
+	Body  string
+	Error string
+}
+
+// CompileResp Playgroundからのレスポンス
+type CompileResp struct {
+	Errors string
+	Events []struct {
+		Message string
+		Kind    string
+		Delay   int
+	}
+	Status      int
+	IsTest      bool
+	TestsFailed int
+}
+
+func request(baseURL string, path string, userAgent string, payload map[string]string, output interface{}) (string, error) {
+	values := url.Values{}
+	for k, v := range payload {
+		values.Set(k, v)
+	}
+	reqURL := fmt.Sprintf("%s/%s", baseURL, path)
+	req, err := http.NewRequest("POST", reqURL, strings.NewReader(values.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Set("User-Agent", userAgent)
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	bodyString := string(bodyBytes)
+	err = json.Unmarshal(bodyBytes, output)
+	return bodyString, err
+}
+
+func formatProgram(program string) (*FormatResp, error) {
+	var output FormatResp
+	_, err := request(playGroundURL, "fmt", userAgent, map[string]string{
+		"imports": "true",
+		"body":    program,
+	}, &output)
+	if err != nil {
+		return nil, err
+	}
+	if len(output.Error) > 0 {
+		return nil, errors.New(output.Error)
+	}
+	return &output, nil
+}
+
+func runProgram(program string) (*CompileResp, error) {
+	var output CompileResp
+	_, err := request(playGroundURL, "compile", userAgent, map[string]string{
+		"version": "2",
+		"body":    program,
+	}, &output)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(output.Events) < 1 {
+		return nil, errors.New(output.Errors)
+	}
+
+	return &output, nil
+}
+
+func main() {
+	idobataAPIToken := os.Getenv("IDOBATA_API_TOKEN")
+	if len(idobataAPIToken) == 0 {
+		log.Fatal("IDOBATA_API_TOKEN was not set.")
+	}
+
+	var onStart idobot.OnStartHandler = func(bot *idobot.Bot, msg *idobot.SeedMsg) {
+		fmt.Printf("[%s] Connection Established.\n", bot.BotName())
+	}
+
+	var onEvent idobot.OnEventHandler = func(bot *idobot.Bot, msg *idobot.EventMsg) {
+		botName := bot.BotName()
+		roomID := msg.Data.Message.RoomID
+		messageBody := msg.Data.Message.BodyPlain
+		fmt.Printf("[%s][%d] Message: %s\n", botName, roomID, messageBody)
+		program := string([]rune(messageBody)[len(botName)+2:])
+		compileOutput, err := runProgram(program)
+		fmt.Printf("[%s] program=%s\n", bot.BotName(), program)
+		if err != nil {
+			bot.PostMessage(roomID, fmt.Sprintf("エラー[compile]: %s", err))
+			return
+		}
+		formatOutput, err := formatProgram(program)
+		if err != nil {
+			bot.PostMessage(roomID, fmt.Sprintf("エラー[format]: %s", err))
+			return
+		}
+		message := fmt.Sprintf("<p><pre>%s</pre></p><details><summary>元のプログラム</summary><pre>%s</pre></details>", compileOutput.Events[0].Message, formatOutput.Body)
+		fmt.Println(message)
+		bot.PostMessage(roomID, message)
+	}
+
+	bot, err := idobot.NewBot(idobataURL, idobataAPIToken, userAgent, onStart, onEvent)
+	if err != nil {
+		log.Fatal("Bot cannot be initialized.")
+	}
+	fmt.Println("Bot was initialized.")
+	bot.Start()
+}
