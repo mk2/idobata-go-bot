18878de07bee14634001997091346d5bd453bcae
Haruka Asakura <haruka.asakura@esm.co.jp>
2019-03-14 19:56:08 +0900

    feat: add test file.

---
+++ bot_test.go
@@ -0,0 +1,20 @@
+package idobot_test
+
+import (
+	"testing"
+
+	"github.com/mk2/idobot"
+)
+
+func TestIdobot_NewBot(t *testing.T) {
+	url := "url"
+	apiToken := "token"
+	userAgent := "userAgent"
+	onStart := func(_ *idobot.Bot, _ *idobot.SeedMsg) {}
+	onEvent := func(_ *idobot.Bot, _ *idobot.EventMsg) {}
+	bot, err := idobot.NewBot(url, apiToken, userAgent, onStart, onEvent)
+
+	if bot == nil || err != nil {
+		t.Errorf("idobot cannot instantiated with NewBot\n")
+	}
+}
