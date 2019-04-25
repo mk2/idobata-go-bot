87444740b76c5d3720594a50abe28f0c1dd8fe8d
Haruka Asakura <haruka.asakura@esm.co.jp>
2019-04-25 16:27:19 +0900

    コミット

--- samples/idobot-gopher/main.go
+++ samples/idobot-gopher/main.go
@@ -99,11 +99,19 @@ func onStart(bot idobot.Bot, msg *idobot.SeedMsg) {
 	fmt.Printf("[%s] Connection Established.\n", bot.BotName())
 }
 
-func onEvent(bot idobot.Bot, msg *idobot.EventMsg) {
+// onHiEvent `@gohper hi`で始まるイベントを処理する
+func onHiEvent(bot idobot.Bot, msg *idobot.EventMsg) {
+	roomID := msg.Data.Message.RoomID
+	senderName := msg.Data.Message.SenderName
+	bot.PostMessage(roomID, fmt.Sprintf("%s、こんにちはだGO。", senderName))
+}
+
+// onProgramEvent デフォルトのイベントハンドラ
+func onProgramEvent(bot idobot.Bot, msg *idobot.EventMsg) {
 	botName := bot.BotName()
 	roomID := msg.Data.Message.RoomID
 	messageBody := msg.Data.Message.BodyPlain
-	fmt.Printf("[%s][%d] Message: %s\n", botName, roomID, messageBody)
+
 	program := string([]rune(messageBody)[len(botName)+2:])
 	compileOutput, err := runProgram(program)
 	fmt.Printf("[%s] program=%s\n", bot.BotName(), program)
@@ -121,6 +129,20 @@ func onEvent(bot idobot.Bot, msg *idobot.EventMsg) {
 	bot.PostMessage(roomID, message)
 }
 
+func onEvent(bot idobot.Bot, msg *idobot.EventMsg) {
+	botName := bot.BotName()
+	roomID := msg.Data.Message.RoomID
+	message := string([]rune(msg.Data.Message.BodyPlain)[len(botName)+2:])
+	fmt.Printf("[%s][%d] Message: %s\n", botName, roomID, message)
+
+	switch {
+	case strings.HasPrefix(message, "hi"):
+		onHiEvent(bot, msg)
+	default:
+		onProgramEvent(bot, msg)
+	}
+}
+
 func onError(bot idobot.Bot, err error) {
 	for roomID := range bot.RoomIDs() {
 		errMsg := fmt.Sprintf("エラー: %+v \n%sは終了しますGO。再起動してGO。", err, bot.BotName())
@@ -130,15 +152,20 @@ func onError(bot idobot.Bot, err error) {
 
 func main() {
 	idobataAPIToken := os.Getenv("IDOBATA_API_TOKEN")
+	storeFilePath := os.Getenv("STORE_FILE_PATH")
 	if len(idobataAPIToken) == 0 {
 		log.Fatal("IDOBATA_API_TOKEN was not set.")
+	}
+
+	if len(storeFilePath) == 0 {
+		log.Fatal("STORE_FILE_PATH was not set.")
 	}
 
 	bot, err := idobot.NewBot(&idobot.NewBotOpts{
 		URL:           idobataURL,
 		APIToken:      idobataAPIToken,
 		UserAgent:     userAgent,
-		StoreFilePath: "/app_data/gopher.store",
+		StoreFilePath: storeFilePath,
 		OnStart:       onStart,
 		OnEvent:       onEvent,
 		OnError:       onError,
