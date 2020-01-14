cf8f2915f05f592012982e6dfdc38d3c11308709
Haruka Asakura <mk2@users.noreply.github.com>
2020-01-14 16:47:00 +0900

    Update start.sh

--- samples/idobot-gopher/start.sh
+++ samples/idobot-gopher/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -e IDOBATA_API_TOKEN=${IDOBATA_API_TOKEN} -e STORE_FILE_PATH=/app_data/gopher.store -d -v `pwd`/app_data:/app_data gopher 
+docker run --restart=always -e IDOBATA_API_TOKEN=${IDOBATA_API_TOKEN} -e STORE_FILE_PATH=/app_data/gopher.store -d -v `pwd`/app_data:/app_data gopher 
